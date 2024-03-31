da6c1b139296ef05acdd6a605eeb2d1aab23cc52
vsantos1711 <vsantos067100@gmail.com>
2024-03-31 01:17:21 -0300

    feat: add required  providers and terraform version

--- 01-primeiro-script/main.tf
+++ 01-primeiro-script/main.tf
@@ -1,12 +1,22 @@
+terraform {
+  required_version = "1.7.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bucket-example" {
+resource "aws_s3_bucket" "my-example-bucktet" {
   bucket = "my-tf-test-bucket-067100"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My terraform test bucket"
     Environment = "Dev"
     Managedby   = "Terraform"
   }
