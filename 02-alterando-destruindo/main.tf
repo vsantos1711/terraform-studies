360e296d58f1cf7bbda4ceeb04aa399254fbf5f1
vsantos1711 <vsantos067100@gmail.com>
2024-04-01 22:27:21 -0300

    feat: change some tags of the s3

---
+++ 02-alterando-destruindo/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.7.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my-example-bucktet" {
+  bucket = "my-tf-test-bucket-067100"
+
+  tags = {
+    Name        = "My terraform test bucket"
+    Environment = "Dev"
+    Managedby   = "Terraform"
+    Owner       = "Vinícius Santos"
+    UpdatedAt   = "2024-04-01"
+  }
+}
